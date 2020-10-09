0785aab3ffe3640e842e5cf069ac39651e8a9318
glc-maida <maida@carboatmedia.fr>
2020-10-09 23:28:02 +0200

    port

--- src/Search.js
+++ src/Search.js
@@ -9,7 +9,7 @@ class Search extends React.Component{
 
 	constructor(props){
 		super(props)
-		this.url_search='http://localhost:8080/search'
+		this.url_search='http://localhost:4000/search'
 		this.state={
 			results: [],
 			suggestions: [],
